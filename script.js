38431884eed8c8c90904e23af84d2ace223394c6
Andy T. <151648189+athara7784@users.noreply.github.com>
2023-12-06 13:19:07 +0000

    I started building the basic framework and designed a simple guessing method in js

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function guess()  {
+    var userGuess = document.getElementById("userInput").value;
+    
+/*  for i number of times (Where i is the number of boxes)
+        if the correct pokemons characteristic completely matches the guessed pokemon
+            turn the ith box green
+        else if the correct pokemons characteristic has at least one match with the guessed pokemon
+            turn the ith box red
+        else 
+            turn the ith box red
+*/
+}
+ 
